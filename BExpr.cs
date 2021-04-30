d03d357fc978c42bf28d4d8286602d1f7f6eeb6e
Nttlong <zugeliang2000@gmail.com>
2021-04-30 16:30:49 +0700

    OK

---
+++ BExpr.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicExpr
+{
+    public class BExpr
+    {
+
+        public string Operator { get; internal set; }
+        public object Left { get; internal set; }
+        public object Right { get; internal set; }
+        public override string ToString()
+        {
+            return "("+Left.ToString()+")"+Operator+"("+Right.ToString()+")";
+        }
+    }
+}
